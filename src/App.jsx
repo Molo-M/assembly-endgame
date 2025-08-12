c760bf4d711a76a9b5f67e26e191c287a98ad644
Molo Munyansanga <molomunyansanga@gmail.com>
2025-08-12 09:51:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function App() {
+  // An array of our chips 
+  const chips = [
+    {name: "HTML", color: "bg-orange-400"},
+    {name: "CSS", color: "bg-blue-500"},
+    {name: "Javascript", color: "bg-yellow-600"},
+    {name: "React", color: "bg-sky-500"},
+    {name: "Typescript", color: "bg-orange-600"},
+    {name: "NodeJS", color: "bg-green-600"},
+    {name: "Python", color: "bg-orange-600"},
+    {name: "Ruby", color: "bg-red-600"},
+    {name: "Assembly", color: "bg-blue-700"}
+  ]
+
+  // Create state to save our current word
+  const [currentWord, setCurrentWord] = useState("React")
+  // Map over word to render it below
+  const wordGuess = [...currentWord].map((item, index) => 
+    <div key={index} className="border-b bg-gray-800 px-3 py-1">{item.toUpperCase()}</div>
+  )
+
+  // Alphabet to store letters 
+  const alphabet = "qwertyuiopasdfghjklzxcvbnm"
+  //Map over alphabet to render it below
+  const keyboard = [...alphabet].map(item => 
+    <button key={item} className="py-1 px-3 cursor-pointer bg-yellow-300 hover:bg-yellow-500 text-black">{item.toUpperCase()}</button>
+  )
+  return (
+    <>
+    <main className="bg-black text-white px-5 py-8 flex flex-col gap-10 items-center">
+      <header className="text-center flex flex-col gap-5">
+        <h1 className="text-2xl">Assembly: Endgame</h1>
+        <p className="text-gray-400">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section id="status" className="bg-green-700 text-center py-5 rounded-xl w-full">
+        <h1 className="text-xl font-semibold">You win!</h1>
+        <p>Well done!</p>
+      </section>
+      <section id="chips" className="flex flex-wrap gap-1 justify-center">
+        {chips.map(item => <div key={item.name} className={`${item.color} text-center py-1 w-20`}>{item.name}</div>)}
+      </section>
+      <section id="word" className="flex gap-1 text-2xl justify-center">
+        {wordGuess}
+      </section>
+      <section id="alphabet" className="flex flex-wrap gap-1 justify-center w-94">
+        {keyboard}
+      </section>
+      <button className="py-1 w-50 bg-sky-700 hover:bg-sky-900 cursor-pointer font-semibold text-xl">New Game</button>
+    </main>
+    </>
+  )
+}
